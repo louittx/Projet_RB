b53b1342d0d93efec032ed716b09a257ef3496dc
louittx <blanclouis16@gmail.com>
2025-05-25 19:12:15 +0200

    test of send emtotion to bluetooth

--- RB_Test/src/main.cpp
+++ RB_Test/src/main.cpp
@@ -2,8 +2,24 @@
 #include <BluetoothSerial.h>
 
 BluetoothSerial SerialBT;
-uint8_t data[] = {0x44, 0x45, 0x46};
+uint8_t data[];
 char buffer[1023];
+
+uint8_t peur;
+uint8_t malade;
+uint8_t hummer;
+uint8_t fatigue;
+uint16_t vie; 
+uint8_t AdresseEmotion = 0x7;
+uint8_t AdresseJeux = 0x3;
+
+void sendEmotion(){
+    data[0] = ((AdresseEmotion << 4)| peur)&0xff;
+    data[1] = ((hummer << 4) | fatigue) & 0xff;
+    data[2] = ((malade << 4) | ((vie>>8)&0xf))& 0xff;
+    data[3] = vie & 0xff;
+
+}
 
 void setup()
 {
@@ -13,6 +29,17 @@ void setup()
     Serial.println("Bluetooth device is ready to pair");
 }
 
+
+void loop()
+{
+    if (SerialBT.hasClient())
+    {
+        SerialBT.write(AdresseEmotion,1);
+    while(SerialBT.hasClient()){
+    }
+}
+
+/*
 void loop()
 {
     if (SerialBT.hasClient())
@@ -37,4 +64,4 @@ void loop()
         Serial.println("No client connected");
         delay(1000);
     }
-}
+}*/
